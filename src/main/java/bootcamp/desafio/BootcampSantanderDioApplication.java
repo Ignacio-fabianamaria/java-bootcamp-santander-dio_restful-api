934561f10177aead15b286e5e398dc3a58a7158d
Siigmaf <faby.tbe@gmail.com>
2023-10-20 09:24:24 -0300

    first commit

---
+++ src/main/java/bootcamp/desafio/BootcampSantanderDioApplication.java
@@ -0,0 +1,13 @@
+package bootcamp.desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampSantanderDioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampSantanderDioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bootcamp/desafio/BootcampSantanderDioApplicationTests.java
@@ -0,0 +1,13 @@
+package bootcamp.desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampSantanderDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
