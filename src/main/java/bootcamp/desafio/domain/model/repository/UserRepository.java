62b5853d3b7b4dac0431c6601dbe2e8cd11f7d36
Siigmaf <faby.tbe@gmail.com>
2023-10-20 10:42:29 -0300

    add UserRepository

---
+++ src/main/java/bootcamp/desafio/domain/model/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package bootcamp.desafio.domain.model.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import bootcamp.desafio.domain.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+    
+}
